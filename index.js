7f4945044540c19c142ba981ba02e51c7c071599
nitins12358 <nitins12358@gmail.com>
2021-02-24 11:54:12 +0530

    uploading

---
+++ index.js
@@ -0,0 +1,128 @@
+// var bellBoy = {
+//   name:"zayn",
+//   age: 24,
+//   experienced:true, 
+//   dropLuggage : ()=>{
+//     alert("dropped the luggage to the room")
+//   }
+// }
+// console.log(bellBoy.dropLuggage())
+
+
+
+
+
+
+// constructor function
+
+// function BellBoy(name,age,experience,languages){
+//   this.fullName = name,
+//   this.age = age,
+//   this.experience = experience,
+//   this.languages = languages 
+// }
+
+// var sumit = new BellBoy("sumit",24,3,["english","hindi"]);
+// var raju = new BellBoy("raju shrivastav", 32, 0, "unknown")
+
+// console.log(raju)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function Housekeeper(name,age,experience){
+//   this.fullname = name,
+//   this.age = age,
+//   this.experience = experience
+// }
+ 
+// var hs1 = new Housekeeper("hs1",67,100);
+
+// console.log(hs1)
+
+
+
+
+
+
+// document.addEventListener("keydown",()=>{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function Housekeeper(name,age,experience){
+//   this.fullname = name,
+//   this.age = age,
+//   this.experience = experience
+// }
+
+// var hs2 = new Housekeeper ("rahul",23,2);
+// console.log(hs2)
+
+
+
+
+
+
+// jquery
+
+
+
+    $("p").hide();
+
